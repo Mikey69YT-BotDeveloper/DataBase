da1dfbbc0877fbb4db1db9a5b01bf36840b9f200
Mikey69YT <miky69yt@gmail.com>
2022-03-10 19:58:24 +0100

    a

--- api/lib/settings.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	port: "80",
-	limitCount: 500,
-	limitPremium: 9999999,
-    site_key: '6LfYgMgeAAAAAKDj0pwsI3y9NHjGn6BNxpMjjw-h',
-    secret_key: '6LfYgMgeAAAAAFTP3EraxQKKw_w6I6sSNz2n6-dK',
-    dbURI: 'mongodb+srv://Mikey69YT:00oliver@mkbot.hsqlt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-};
